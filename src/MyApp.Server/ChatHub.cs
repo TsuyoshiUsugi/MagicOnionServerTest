94d881b703c6506e54f2964d0f4af964891d0d61
TsuyoshiUsugi <qesulive@gmail.com>
2025-06-11 06:22:30 +0900

    初コミット

---
+++ src/MyApp.Server/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using MagicOnion.Server.Hubs;
+using MyApp.Shared;
+using MessagePack.Unity;
+using UnityEngine;
+
+namespace MyApp.Server
+{
+    public class ChatHub : StreamingHubBase<IChatHub, IChatHubReceiver>, IChatHub
+    {
+        IGroup<IChatHubReceiver>? room;
+        string userName = "unknown";
+
+        public async ValueTask JoinAsync(string roomName, string userName)
+        {
+            Console.WriteLine($"JoinAsync called: {userName} joined {roomName}");
+            this.room = await Group.AddAsync(roomName);
+            this.userName = userName;
+            room.All.OnJoin(userName);
+        }
+
+        public async ValueTask LeaveAsync()
+        {
+            room.All.OnLeave(userName);
+            await room.RemoveAsync(Context);
+        }
+
+        public async ValueTask SendMessageAsync(string message)
+        {
+            room.All.OnSendMessage(userName, message);
+        }
+    }
+}
---
+++ src/MyApp.Shared/IChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MagicOnion;
+
+namespace MyApp.Shared
+{
+    public interface IChatHub : IStreamingHub<IChatHub, IChatHubReceiver>
+    {
+        ValueTask JoinAsync(string roomName, string userName);
+        ValueTask LeaveAsync();
+        ValueTask SendMessageAsync(string message);
+    }
+    
+    public interface IChatHubReceiver
+    {
+        void OnJoin(string userName);
+        void OnLeave(string userName);
+        void OnSendMessage(string userName, string message);
+    }
+}
